2946dbe186f015e3127c7b9617c676023f27d6ee
Nastya Pisanko <nana.pisanka@gmail.com>
2026-06-01 11:56:37 +0300

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+      vue(),
+    vueDevTools(),
+    createHtmlPlugin({}),
+    createHtmlPlugin({})
+  ],
+  base: {
+    base: '/meal-planner/',
+  }
+})
